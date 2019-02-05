a2712ac29ace81091655c1af4b9edde9cf82803e
RWGarland <rupertwilliamgarland@gmail.com>
2019-02-05 19:50:51 +0000

    Adding Files so far the project
    Adding the main movement of the game

---
+++ SoundRecognition/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+/*
+ * Allows the user to use the mouse to move items around the canvas
+ * DragHandler does throw up some errors. I am unsure how to find the solution.
+ *  This has been placed in the reference placed. 
+ */
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject item;
+
+
+    Transform startParent;
+    Vector3 startPosition;
+    bool start = true;
+
+
+    public void OnBeginDrag(PointerEventData eventData) //Allows the user to get infotion on what they can grab
+    {
+        item = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false; //Using the canvus allows me to grab items and then move them using the mouse
+      
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        transform.position = Input.mousePosition; //where the mouse or in this case finger will take the sprite.
+    }
+
+    public void OnEndDrag(PointerEventData eventData) //if dropped outside of its area this will make it go back to its orgenal position.
+    {
+        item = null;
+
+        if (transform.parent == startParent)
+        {
+            transform.position = startPosition; //once the child has been moved it will stay there
+        }
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+    }
+
+}
+
---
+++ SoundRecognition/Assets/Scripts/Slot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+/*
+ * This works with the drag handler to allow the child of a parent to be able to move.
+ * In the reference page*/
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+
+    private Stack<IInventoryItem> mitemStack = new Stack<IInventoryItem>()
+
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 4) //if there is no object there
+            {
+                return transform.GetChild(4).gameObject; // this will put the child back to where the parent is so it doesnt just hover in empty space
+            }
+            return null;
+        }
+    }
+    #region IdropHandler implementation
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandler.item.transform.SetParent(transform); //changes where the child obeject goes so it can stay where it is.
+        }
+    }
+    #endregion
+
+}
